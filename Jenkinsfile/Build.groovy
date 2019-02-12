e3bef67af6fa5bfc4023f1eae1f12a53ac058dc3
Nathan Blore <37145422+nblore@users.noreply.github.com>
2019-02-12 18:28:09 +0000

    Update Build.groovy

--- Jenkinsfile/Build.groovy
+++ Jenkinsfile/Build.groovy
@@ -64,7 +64,7 @@ podTemplate(label: label, containers: [
                 sh "mkdir ${LBRANCH}"
                 sh "cp -r src/* ${LBRANCH}/"
                 sh "cp -r node_modules ${LBRANCH}/node_modules"
-                sh "zip -qr ${LBRANCH}.zip ${LBRANCH}"
+                sh "cd ${LBRANCH} && zip -qr ../${LBRANCH}.zip *"
             }
 
             stage("upload to s3") {
@@ -74,6 +74,8 @@ podTemplate(label: label, containers: [
                                   secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
 
                     sh "aws s3 cp ${LBRANCH}.zip s3://cvs-services/test-types/${LBRANCH}.zip"
+                    sh "echo sha256sum ${LBRANCH}.zip > ${LBRANCH}.sha256 && aws s3 cp ${LBRANCH}.sha256 s3://cvs-services/test-stations/${LBRANCH}.sha256"
+
                 }
             }
         }
