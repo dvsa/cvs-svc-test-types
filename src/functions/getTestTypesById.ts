d7c3e1f3818c9068ce4d7483283745091eae9cb0
oalexandru <oalexandru@deloittece.com>
2019-08-21 15:50:11 +0100

    Added function to parse missing query params

--- src/functions/getTestTypesById.ts
+++ src/functions/getTestTypesById.ts
@@ -5,40 +5,50 @@ import Joi from "joi";
 import { Handler } from "aws-lambda";
 
 export const getTestTypesById: Handler = (event, context, callback) => {
-  const testTypesDAO = new TestTypesDAO();
-  const testTypesService = new TestTypesService(testTypesDAO);
+  const testTypesDAO = new TestTypesDAO()
+  const testTypesService = new TestTypesService(testTypesDAO)
 
   // Validate query parameters
   const queryParamSchema = Joi.object().keys({
     fields: Joi.string().regex(/^(testTypeClassification|defaultTestCode|linkedTestCode),?\s*((testTypeClassification|defaultTestCode|linkedTestCode),?\s*)?((testTypeClassification|defaultTestCode|linkedTestCode),?\s*)?$/).required(),
-    vehicleType: Joi.any().only([ "psv", "hgv", "trl" ]).required(),
-    vehicleSize: Joi.any().only([ "small", "large" ]),
-    vehicleConfiguration: Joi.any().only([ "rigid", "articulated" ]).required(),
+    vehicleType: Joi.any().only([ 'psv', 'hgv', 'trl' ]).required(),
+    vehicleSize: Joi.any().only([ 'small', 'large' ]),
+    vehicleConfiguration: Joi.any().only([ 'rigid', 'articulated' ]).required(),
     vehicleAxles: Joi.number().min(0).max(99).allow(null).optional()
-  });
+  })
 
-  const queryParams = Object.assign({}, event.queryStringParameters);
-  if (queryParams.vehicleAxles) {
-    if (queryParams.vehicleAxles === "null") {
-      queryParams.vehicleAxles = null;
-    } else {
-      queryParams.vehicleAxles = parseInt(queryParams.vehicleAxles, 10);
-    }
-  }
-  const validation = Joi.validate(queryParams, queryParamSchema);
+  let queryParams = parseMissingQueryParams(event.queryStringParameters)
+
+  let validation = Joi.validate(queryParams, queryParamSchema)
 
   if (validation.error) {
-    return Promise.resolve(new HTTPResponse(400, `Query parameter ${validation.error.details[0].message}`));
+    return Promise.resolve(new HTTPResponse(400, `Query parameter ${validation.error.details[0].message}`))
   }
 
   // Splitting fields into an array and cleaning up unwanted whitespace
-  Object.assign(queryParams, { fields: queryParams.fields.replace(/\s/g, "").split(",") });
+  Object.assign(queryParams, { fields: queryParams.fields.replace(/\s/g, '').split(',') })
 
   return testTypesService.getTestTypesById(event.pathParameters.id, queryParams)
-    .then((data) => {
-      return new HTTPResponse(200, data);
-    })
-    .catch((error) => {
-      return new HTTPResponse(error.statusCode, error.body);
-    });
-};
+      .then((data) => {
+        return new HTTPResponse(200, data)
+      })
+      .catch((error) => {
+        return new HTTPResponse(error.statusCode, error.body)
+      })
+}
+
+const parseMissingQueryParams = (queryStringParameters: any) => {
+  let queryParams = Object.assign({}, queryStringParameters)
+  if (queryParams.vehicleAxles) {
+    if (queryParams.vehicleAxles === 'null') {
+      queryParams.vehicleAxles = null
+    } else {
+      queryParams.vehicleAxles = parseInt(queryParams.vehicleAxles)
+    }
+  }
+  if (queryParams.vehicleSize === 'undefined') {
+    queryParams.vehicleSize = null
+  }
+  return queryParams
+}
+
