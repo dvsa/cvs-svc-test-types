7acb75de812ce90335c1a29ac6a2740683fbf777
Arup Banerjee <arupdvsa@alanaamy.net>
2019-08-23 13:30:31 +0100

    fix tslint identified issues

--- src/utils/Configuration.ts
+++ src/utils/Configuration.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import * as yml from "node-yaml";
-import {ERRORS} from "../assets/Enums";
-import {IFunctionEvent} from "./IFunctionEvent"
+import { ERRORS } from "../assets/Enums";
+import { IFunctionEvent } from "./IFunctionEvent";
 import { Handler } from "aws-lambda";
 
 class Configuration {
@@ -9,14 +9,14 @@ class Configuration {
   private static instance: Configuration;
   private readonly config: any;
 
-  constructor (configPath: string) {
-    if (!process.env.BRANCH) {throw new Error(ERRORS.NoBranch); }
-    this.config = yml.readSync(configPath)
+  constructor(configPath: string) {
+    if (!process.env.BRANCH) { throw new Error(ERRORS.NoBranch); }
+    this.config = yml.readSync(configPath);
 
     // Replace environment variable references
-    let stringifiedConfig:string = JSON.stringify(this.config)
-    const envRegex:RegExp = /\${(\w+\b):?(\w+\b)?}/g
-    const matches: RegExpMatchArray | null = stringifiedConfig.match(envRegex)
+    let stringifiedConfig: string = JSON.stringify(this.config);
+    const envRegex: RegExp = /\${(\w+\b):?(\w+\b)?}/g;
+    const matches: RegExpMatchArray | null = stringifiedConfig.match(envRegex);
 
     if (matches) {
       matches.forEach((match) => {
@@ -24,45 +24,45 @@ class Configuration {
         const captureGroups: RegExpExecArray = envRegex.exec(match) as RegExpExecArray;
 
         // Insert the environment variable if available. If not, insert placeholder. If no placeholder, leave it as is.
-        stringifiedConfig = stringifiedConfig.replace(match, (process.env[captureGroups[1]] || captureGroups[2] || captureGroups[1]))
-      })
+        stringifiedConfig = stringifiedConfig.replace(match, (process.env[captureGroups[1]] || captureGroups[2] || captureGroups[1]));
+      });
     }
 
-    this.config = JSON.parse(stringifiedConfig)
+    this.config = JSON.parse(stringifiedConfig);
   }
 
   /**
      * Retrieves the singleton instance of Configuration
      * @returns Configuration
-     */
-  public static getInstance (): Configuration {
+  */
+  public static getInstance(): Configuration {
     if (!this.instance) {
-      this.instance = new Configuration('../config/config.yml')
+      this.instance = new Configuration("../config/config.yml");
     }
 
-    return Configuration.instance
+    return Configuration.instance;
   }
 
   /**
      * Retrieves the entire config as an object
      * @returns any
-     */
-  public getConfig (): any {
-    return this.config
+  */
+  public getConfig(): any {
+    return this.config;
   }
 
   /**
      * Retrieves the lambda functions declared in the config
      * @returns IFunctionEvent[]
-     */
-  public getFunctions (): IFunctionEvent[]  {
+  */
+  public getFunctions(): IFunctionEvent[] {
     if (!this.config.functions) {
-      throw new Error('Functions were not defined in the config file.')
+      throw new Error("Functions were not defined in the config file.");
     }
 
     return this.config.functions.map((fn: Handler) => {
-      const [name, params] = Object.entries(fn)[0]
-      const path = (params.proxy) ? params.path.replace('{+proxy}', params.proxy) : params.path
+      const [name, params] = Object.entries(fn)[0];
+      const path = (params.proxy) ? params.path.replace("{+proxy}", params.proxy) : params.path;
 
       return {
         name,
@@ -70,34 +70,34 @@ class Configuration {
         path,
         function: require(`../functions/${name}`)[name],
         event: params.event
-      }
-    })
+      };
+    });
   }
 
   /**
      * Retrieves the DynamoDB config
      * @returns any
      */
-  public getDynamoDBConfig (): any {
+  public getDynamoDBConfig(): any {
     if (!this.config.dynamodb) {
-      throw new Error('DynamoDB config is not defined in the config file.')
+      throw new Error("DynamoDB config is not defined in the config file.");
     }
 
     // Not defining BRANCH will default to remote
-    let env
+    let env;
     switch (process.env.BRANCH) {
-      case 'local':
-        env = 'local'
-        break
-      case 'local-global':
-        env = 'local-global'
-        break
+      case "local":
+        env = "local";
+        break;
+      case "local-global":
+        env = "local-global";
+        break;
       default:
-        env = 'remote'
+        env = "remote";
     }
 
-    return this.config.dynamodb[env]
+    return this.config.dynamodb[env];
   }
 }
 
-export { Configuration};
+export { Configuration };
