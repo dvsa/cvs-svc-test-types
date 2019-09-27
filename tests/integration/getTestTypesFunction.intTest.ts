9eaad85e7f5c8fc878dcf2d0d8b87e2d842372c7
Bhavana Siddappa Hemanth <bhemanth@deloitte.co.uk>
2019-09-27 12:07:41 +0100

    CVSB-7968: Remove non-jest test frameworks in int tests.

--- tests/integration/getTestTypesFunction.intTest.ts
+++ tests/integration/getTestTypesFunction.intTest.ts
@@ -1,15 +1,12 @@
-it("Fake Test 3", () => { expect(true); });
+import LambdaTester from "lambda-tester";
+import { handler } from "../../src/handler";
 
-// import LambdaTester from "lambda-tester";
-// import { handler } from "../../src/handler";
-// import {expect} from "chai";
-//
-// //This test is garbage and doesn't actually test anything. Should be replaced with an actual test.
-// describe("getTestTypes", () => {
-//   it("should return a promise", () => {
-//     const lambda = LambdaTester(handler);
-//     return lambda.expectResolve((response: any) => {
-//       expect(response).to.exist;
-//     });
-//   });
-// });
+//This test is garbage and doesn't actually test anything. Should be replaced with an actual test.
+describe("getTestTypes", () => {
+  it("should return a promise", () => {
+    const lambda = LambdaTester(handler);
+    return lambda.expectResolve((response: any) => {
+      expect(response).toBeTruthy();
+    });
+  });
+});
