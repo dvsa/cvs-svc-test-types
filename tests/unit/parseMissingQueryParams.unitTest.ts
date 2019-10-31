1c925c295b5114b905bd828dbb0691127fd27fba
khodder <khodder@gmail.com>
2019-10-31 13:17:48 +0000

    CVASB-8243 add test for changed parseMissingQueryParams

--- tests/unit/parseMissingQueryParams.unitTest.ts
+++ tests/unit/parseMissingQueryParams.unitTest.ts
@@ -2,14 +2,14 @@ import {parseMissingQueryParams} from "../../src/utils/parseMissingQueryParams";
 
 describe("parseMissingQueryParams", () => {
     context("if queryStringParameters does not contain vehicleSize", () => {
-        it("vehicleSize should be returned as null", () => {
+        it("vehicleSize should not be added", () => {
 
             const queryStringParametersWithoutVehicleSize = {
                 vehicleAxles: 3,
                 vehicleConfiguration: "rigid"
             };
 
-            expect(parseMissingQueryParams(queryStringParametersWithoutVehicleSize).vehicleSize).toEqual(null);
+            expect(Object.keys(parseMissingQueryParams(queryStringParametersWithoutVehicleSize))).not.toContain("vehicleSize");
         });
     });
 
