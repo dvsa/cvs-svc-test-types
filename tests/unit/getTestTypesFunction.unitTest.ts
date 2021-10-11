75ab3ab6e91e800bc5c6a3d5f99b9331bf52120d
Daniel Searle <84069850+Daniel-Searle@users.noreply.github.com>
2021-10-11 14:44:54 +0100

    Feature/cvsb 19434 (#109)
    * feat(cvsb-19434): added suggested test types
    
    * feat(cvsb-19434): added additional unit test
    
    * feat(cvsb-19434): prittier format
    
    * feat(cvsb-19434): added suggested test types to swagger doc
    
    * feat(cvsb-19434): prittier
    
    * feat(cvsb-19434): fixed unit tests
    
    * feat(cvsb-19434): removal of redundant test
    
    * feat(cvsb-19434): removal of suggested test type ids from the test types api yml
    
    * feat(cvsb-19434): added an additional unit test
    
    * feat(cvsb-19434): prittier
    
    * feat(cvsb-19434): updated unit test, removed docs from prittier ignore
    
    Co-authored-by: Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>

--- tests/unit/getTestTypesFunction.unitTest.ts
+++ tests/unit/getTestTypesFunction.unitTest.ts
@@ -2,6 +2,8 @@ import { getTestTypes } from "../../src/functions/getTestTypes";
 import { TestTypesService } from "../../src/services/TestTypesService";
 import { HTTPError } from "../../src/models/HTTPError";
 import { Context } from "aws-lambda";
+import TestTypes from "../resources/test-types.json";
+import { cloneDeep } from "lodash";
 
 describe("getTestTypes function", () => {
   // @ts-ignore
@@ -18,6 +20,53 @@ describe("getTestTypes function", () => {
       expect(output.body).toEqual(JSON.stringify("Success"));
     });
   });
+
+  context(
+    "gets successful response with suggested test types from Service",
+    () => {
+      it("returns 200 OK + data", async () => {
+        const mockTestTypesRecord = cloneDeep(TestTypes[0]);
+
+        TestTypesService.prototype.getTestTypesList = jest
+          .fn()
+          .mockResolvedValue(mockTestTypesRecord);
+        const output = await getTestTypes({}, ctx, () => {
+          return;
+        });
+        const {
+          id,
+          linkedIds,
+          suggestedTestTypeIds,
+          name,
+          testTypeName,
+          forVehicleType,
+          forVehicleSize,
+          forVehicleConfiguration,
+          forVehicleAxles,
+          forEuVehicleCategory,
+          forVehicleClass,
+          forVehicleSubclass,
+          forVehicleWheels,
+          testTypeClassification,
+        } = JSON.parse(output.body);
+        expect(output.statusCode).toEqual(200);
+        expect(id).toEqual("1");
+        expect(linkedIds).toEqual(["38", "39"]);
+        expect(suggestedTestTypeIds).toEqual(["1", "7", "10"]);
+        expect(name).toEqual("Annual test");
+        expect(testTypeName).toEqual("Annual test");
+        expect(forVehicleType).toEqual(["psv"]);
+        expect(forVehicleSize).toEqual(["small", "large"]);
+        expect(forVehicleConfiguration).toEqual(["articulated", "rigid"]);
+        expect(forVehicleAxles).toEqual(null);
+        expect(forEuVehicleCategory).toEqual(null);
+        expect(forVehicleClass).toEqual(null);
+        expect(forVehicleSubclass).toEqual(null);
+        expect(forVehicleWheels).toEqual(null);
+        expect(testTypeClassification).toEqual("Annual With Certificate");
+      });
+    }
+  );
 
   context("gets error from Service", () => {
     it("Returns the error as an HTTPResponse", async () => {
