d2f888757dba37e256d628c35dd683d63fa9d291
Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
2022-06-21 12:08:14 +0100

    feat(VTA-696): uncommented out tests

--- tests/unit/getTestTypesByIDFunction.unitTest.ts
+++ tests/unit/getTestTypesByIDFunction.unitTest.ts
@@ -10,136 +10,136 @@ describe("getTestTypesById Function", () => {
 
   context("with good event but invalid details", () => {
     it("should return error from Joi", async () => {
-      // TestTypesService.prototype.getTestTypesById = jest
-      //   .fn()
-      //   .mockResolvedValue("Success");
-      // const myEvent = {
-      //   httpMethod: "GET",
-      //   path: "/test-types/1",
-      //   queryStringParameters: {
-      //     fields: "testTypeClassification",
-      //     vehicleType: "hgv",
-      //     vehicleSize: "small",
-      //     vehicleConfiguration: "notAConfiguration",
-      //   },
-      //   pathParameters: {
-      //     id: 1,
-      //   },
-      // };
-      // const result = await getTestTypesById(myEvent, ctx, () => {
-      //   return;
-      // });
-      // expect(result).toBeInstanceOf(HTTPResponse);
-      // expect(result.statusCode).toEqual(400);
-      // expect(result.body).toEqual(
-      //   JSON.stringify(
-      //     'Query parameter "vehicleConfiguration" must be one of [articulated, rigid, centre axle drawbar, semi-car transporter, semi-trailer, low loader, other, drawbar, four-in-line, dolly, full drawbar, null]'
-      //   )
-      // );
+      TestTypesService.prototype.getTestTypesById = jest
+        .fn()
+        .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {
+          fields: "testTypeClassification",
+          vehicleType: "hgv",
+          vehicleSize: "small",
+          vehicleConfiguration: "notAConfiguration",
+        },
+        pathParameters: {
+          id: "1",
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(400);
+      expect(result.body).toEqual(
+        JSON.stringify(
+          'Query parameter "vehicleConfiguration" must be one of [articulated, rigid, centre axle drawbar, semi-car transporter, semi-trailer, low loader, other, drawbar, four-in-line, dolly, full drawbar, null]'
+        )
+      );
     });
   });
 
   context("with good event - non PSV details", () => {
     it("should return data from service on Success", async () => {
-      // TestTypesService.prototype.getTestTypesById = jest
-      //   .fn()
-      //   .mockResolvedValue("Success");
-      // const myEvent = {
-      //   httpMethod: "GET",
-      //   path: "/test-types/1",
-      //   queryStringParameters: {
-      //     fields: "testTypeClassification",
-      //     vehicleType: "hgv",
-      //     vehicleSize: "small",
-      //     vehicleConfiguration: "drawbar",
-      //   },
-      //   pathParameters: {
-      //     id: 1,
-      //   },
-      // };
-      // const result = await getTestTypesById(myEvent, ctx, () => {
-      //   return;
-      // });
-      // expect(result).toBeInstanceOf(HTTPResponse);
-      // expect(result.statusCode).toEqual(200);
-      // expect(result.body).toEqual(JSON.stringify("Success"));
+      TestTypesService.prototype.getTestTypesById = jest
+        .fn()
+        .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {
+          fields: "testTypeClassification",
+          vehicleType: "hgv",
+          vehicleSize: "small",
+          vehicleConfiguration: "drawbar",
+        },
+        pathParameters: {
+          id: "1",
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(200);
+      expect(result.body).toEqual(JSON.stringify("Success"));
     });
   });
 
   context("with good event", () => {
     it("should return data from service on Success", async () => {
-      // TestTypesService.prototype.getTestTypesById = jest
-      //   .fn()
-      //   .mockResolvedValue("Success");
-      // const myEvent = {
-      //   httpMethod: "GET",
-      //   path: "/test-types/1",
-      //   queryStringParameters: {
-      //     fields: "testTypeClassification",
-      //     vehicleType: "psv",
-      //     vehicleSize: "small",
-      //     vehicleConfiguration: "rigid",
-      //   },
-      //   pathParameters: {
-      //     id: 1,
-      //   },
-      // };
-      // const result = await getTestTypesById(myEvent, ctx, () => {
-      //   return;
-      // });
-      // expect(result).toBeInstanceOf(HTTPResponse);
-      // expect(result.statusCode).toEqual(200);
-      // expect(result.body).toEqual(JSON.stringify("Success"));
+      TestTypesService.prototype.getTestTypesById = jest
+        .fn()
+        .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {
+          fields: "testTypeClassification",
+          vehicleType: "psv",
+          vehicleSize: "small",
+          vehicleConfiguration: "rigid",
+        },
+        pathParameters: {
+          id: "1",
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(200);
+      expect(result.body).toEqual(JSON.stringify("Success"));
     });
 
     it("should RETURN the error on Service Failure", async () => {
-      // const myError = new HTTPError(418, "It broke!");
-      // TestTypesService.prototype.getTestTypesById = jest
-      //   .fn()
-      //   .mockRejectedValue(myError);
-      // const myEvent = {
-      //   httpMethod: "GET",
-      //   path: "/test-types/1",
-      //   queryStringParameters: {
-      //     fields: "testTypeClassification",
-      //     vehicleType: "psv",
-      //     vehicleSize: "small",
-      //     vehicleConfiguration: "rigid",
-      //   },
-      //   pathParameters: {
-      //     id: 1,
-      //   },
-      // };
-      // const result = await getTestTypesById(myEvent, ctx, () => {
-      //   return;
-      // });
-      // expect(result).toBeInstanceOf(HTTPResponse);
-      // expect(result.statusCode).toEqual(418);
-      // expect(result.body).toEqual(JSON.stringify("It broke!"));
+      const myError = new HTTPError(418, "It broke!");
+      TestTypesService.prototype.getTestTypesById = jest
+        .fn()
+        .mockRejectedValue(myError);
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {
+          fields: "testTypeClassification",
+          vehicleType: "psv",
+          vehicleSize: "small",
+          vehicleConfiguration: "rigid",
+        },
+        pathParameters: {
+          id: "1",
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(418);
+      expect(result.body).toEqual(JSON.stringify("It broke!"));
     });
   });
 
   context("with missing query params", () => {
     it("should return data from service", async () => {
-      // TestTypesService.prototype.getTestTypesById = jest
-      //   .fn()
-      //   .mockResolvedValue("Success");
-      // const myEvent = {
-      //   httpMethod: "GET",
-      //   path: "/test-types/1",
-      //   queryStringParameters: {},
-      //   pathParameters: {
-      //     id: 1,
-      //   },
-      // };
-      // const result = await getTestTypesById(myEvent, ctx, () => {
-      //   return;
-      // });
-      // expect(result).toBeInstanceOf(HTTPResponse);
-      // expect(result.statusCode).toEqual(400);
-      // expect(result.body).toEqual(
-      //   JSON.stringify('Query parameter "fields" is required')
-      // );
+      TestTypesService.prototype.getTestTypesById = jest
+        .fn()
+        .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {},
+        pathParameters: {
+          id: "1",
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(400);
+      expect(result.body).toEqual(
+        JSON.stringify('Query parameter "fields" is required')
+      );
     });
   });
 });
