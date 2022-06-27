1af824434050a0331f817183cd00b8b3ff4e3b52
Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
2022-06-27 12:31:53 +0100

    feat(VTA-696): added unit tests

--- tests/unit/getTestTypesByIDFunction.unitTest.ts
+++ tests/unit/getTestTypesByIDFunction.unitTest.ts
@@ -3,6 +3,7 @@ import { TestTypesService } from "../../src/services/TestTypesService";
 import { Context } from "aws-lambda";
 import { HTTPResponse } from "../../src/models/HTTPResponse";
 import { HTTPError } from "../../src/models/HTTPError";
+import {HTTPRESPONSE} from "../../src/assets/Enums";
 
 describe("getTestTypesById Function", () => {
   // @ts-ignore
@@ -139,6 +140,92 @@ describe("getTestTypesById Function", () => {
       expect(result.statusCode).toEqual(400);
       expect(result.body).toEqual(
         JSON.stringify('Query parameter "fields" is required')
+      );
+    });
+  });
+  context("with path params as null", () => {
+    it("should trigger validation in function", async () => {
+      TestTypesService.prototype.getTestTypesById = jest
+        .fn()
+        .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {},
+        pathParameters: null,
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(400);
+      expect(result.body).toEqual(
+          JSON.stringify(HTTPRESPONSE.MISSING_PARAMETERS)
+      );
+    });
+  });
+  context("with path params", () => {
+    it("should trigger validation in function when id is null", async () => {
+      TestTypesService.prototype.getTestTypesById = jest
+          .fn()
+          .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {},
+        pathParameters: {
+          id: null,
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(400);
+      expect(result.body).toEqual(
+          JSON.stringify(HTTPRESPONSE.MISSING_PARAMETERS)
+      );
+    });
+    it("should trigger validation in function when id is undefined", async () => {
+      TestTypesService.prototype.getTestTypesById = jest
+          .fn()
+          .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {},
+        pathParameters: {
+          id: undefined,
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(400);
+      expect(result.body).toEqual(
+          JSON.stringify(HTTPRESPONSE.MISSING_PARAMETERS)
+      );
+    });
+    it("should trigger validation in function when id is an empty string", async () => {
+      TestTypesService.prototype.getTestTypesById = jest
+          .fn()
+          .mockResolvedValue("Success");
+      const myEvent = {
+        httpMethod: "GET",
+        path: "/test-types/1",
+        queryStringParameters: {},
+        pathParameters: {
+          id: " ",
+        },
+      };
+      const result = await getTestTypesById(myEvent, ctx, () => {
+        return;
+      });
+      expect(result).toBeInstanceOf(HTTPResponse);
+      expect(result.statusCode).toEqual(400);
+      expect(result.body).toEqual(
+          JSON.stringify(HTTPRESPONSE.MISSING_PARAMETERS)
       );
     });
   });
