c7a2764e1c568290a4d94f5af713dff7aff0cf21
owen <owen.corrigan@bjss.com>
2024-03-12 16:51:38 +0000

    feat(cb2-11222): changes to start script

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,4 +1,4 @@
-import { spawn } from "child_process";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 
 // We hook to serverless offline when firing its process
 const SERVER_OK = `Server ready: http://localhost:3010`;
@@ -7,47 +7,43 @@ const SERVER_OK = `Server ready: http://localhost:3010`;
 // we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8006
 const DYNAMO_LOCAL_ERROR_THREAD = `Exception in thread "main"`;
 
-const setupServer = (process: any) => new Promise((resolve, reject) => {
-    process.stdout.setEncoding("utf-8").on("data", (stream: any) => {
-      console.log(`stdout: ${stream}`);
+const setupServer = (process: ChildProcessWithoutNullStreams) => new Promise<ChildProcessWithoutNullStreams>(
+  (resolve: (test: ChildProcessWithoutNullStreams) => void) => {
+    process.stdout.setEncoding('utf-8').on('data', (stream: string) => {
       if (stream.includes(SERVER_OK)) {
         resolve(process);
       }
     });
 
-    process.stderr.setEncoding("utf-8").on("data", (stream: any) => {
-      console.log(`stderr: ${stream}`);
-      if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
-        throw new Error("Internal Java process crashed");
-      }
-      else if (stream.includes(SERVER_OK)) {
+    process.stderr.setEncoding('utf-8').on('data', (stream: string) => {
+      if (stream.includes(SERVER_OK)) {
         resolve(process);
       }
     });
 
-    process.on("exit", (code: any, signal: any) => {
+    process.on('exit', (code: number, signal: string) => {
       if (code !== 137) {
-          console.info(`process terminated with code: ${code} and signal: ${signal}`);
+        console.info(
+          `process terminated with code: ${code} and signal: ${signal}`,
+        );
       }
     });
-  });
+  },
+);
 
-const server = spawn("npm", ["run", "start"], {});
-
-// setupServer(server);
+const server = spawn('npm', ['run', 'start'], {});
 
 module.exports = async () => {
-  console.log(`\nSetting up Integration tests...\n\n`);
   try {
+    console.log('\nSetting up Integration tests...\n');
     const instance = await setupServer(server);
-    // @ts-ignore
     const { pid } = instance;
     console.info(`
     start script running ✅ ...
-    on pid: ${pid}
+    on pid: ${pid ?? ''}
     `);
   } catch (e) {
-    console.error("Something wrong happened:\n");
+    console.error('Something wrong happened:\n');
     console.error(e);
     process.exit(1);
   }
